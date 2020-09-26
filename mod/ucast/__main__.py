d0fe9a5fc9a1ed1e4efa21e9e32cd4044f5014b0
Chi-kwan Chan <chanc@arizona.edu>
2020-09-26 01:05:24 -0700

    Add an entry point

--- mod/ucast/__main__.py
+++ mod/ucast/__main__.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-#
 # Copyright (C) 2020 Chi-kwan Chan
 # Copyright (C) 2020 Steward Observatory
 #
@@ -18,25 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with `ucast`.  If not, see <http://www.gnu.org/licenses/>.
 
-from setuptools import setup, find_packages
+def ucast():
+    print('µcast')
 
-setup(
-    name='ucast',
-    version='0.1.0',
-    url='https://github.com/focisrc/ucast',
-    author='Chi-kwan Chan',
-    author_email='chanc@arizona.edu',
-    description='Micro-weather forecasting for astronomy',
-    packages=find_packages('mod'),
-    package_dir={'': 'mod'},
-    entry_points={
-        'console_scripts': [
-            'ucast = ucast.__main__:ucast',
-        ],
-    },
-    python_requires='>=3.6', # `ucast` uses python3's f-string and typing
-    install_requires=[
-        'matplotlib>=3.2',
-        'numpy>=1.19',
-    ],
-)
+if __name__ == '__main__':
+    ucast()
