7a5af7d3d5c87150c1a8e008a3b563c65f5ab0a6
Chi-kwan Chan <chanc@arizona.edu>
2020-09-26 23:30:49 -0700

    Implement AM.solve()

--- mod/ucast/backend/am.py
+++ mod/ucast/backend/am.py
@@ -18,8 +18,22 @@
 
 import shutil
 import subprocess
+import re
 
 class AM:
+
+    # Column density units (cm^-2 equivalents)
+    MM_PWV   = 3.3427e21
+    KG_ON_M2 = 3.3427e21
+    DU       = 2.6868e16
+
+    cmap = {
+        'pwv': (re.compile('^#.*h2o'             ), MM_PWV  ),
+        'lwp': (re.compile('^#.*lwp_abs_Rayleigh'), KG_ON_M2),
+        'iwp': (re.compile('^#.*iwp_abs_Rayleigh'), KG_ON_M2),
+        'o3' : (re.compile('^#.*o3'              ), DU      ),
+    }
+
     def __init__(self, path=None):
         if path is None:
             path = shutil.which('am')
@@ -29,4 +43,21 @@ class AM:
         self.am = path
 
     def run(self, *args, **kwargs):
-        subprocess.run([self.am, *args], **kwargs)
+        return subprocess.run([self.am, *args], **kwargs)
+
+    def solve(self, cfg):
+        res = self.run('-', input=cfg, encoding='utf-8', capture_output=True)
+
+        sol = {}
+
+        l = res.stdout.split()
+        sol['tau'] = float(l[1])
+        sol['Tb' ] = float(l[2])
+
+        for l in res.stderr.split('\n'):
+            for k, (p, f) in self.cmap.items():
+                if p.match(l):
+                    sol[k] = float(l.split()[2]) / f
+                    continue
+
+        return sol
