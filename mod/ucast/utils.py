9e6caeaa01517962e6322c8fd90808cd90e7ff60
Chi-kwan Chan <chanc@arizona.edu>
2020-09-26 22:27:31 -0700

    Implement latest_gfs_cycle_time() using relative_gfs_cycle_time()

--- mod/ucast/utils.py
+++ mod/ucast/utils.py
@@ -16,32 +16,10 @@
 # You should have received a copy of the GNU General Public License
 # along with `ucast`.  If not, see <http://www.gnu.org/licenses/>.
 
-import datetime
-
-def latest_gfs_cycle_time(lag=6):
-    """
-    Get the datetime corresponding to the most recent GFS cycle time.
-
-    Args:
-        lag: The default GFS forecast production lag is 6 hours.  To
-            get an earlier forecast, this might be set somewhat
-            shorter, with due care taken to ensure it isn't set too
-            short.
-
-    Returns:
-        The most recent GFS cycle time.
-
-    """
-    now = datetime.datetime.utcnow()
-    lag = datetime.timedelta(hours=lag)
-    gfs = now - lag
-    return gfs.replace(hour=gfs.hour//6*6,
-                       minute=0,
-                       second=0,
-                       microsecond=0)
+from datetime import datetime, timedelta
 
 def relative_gfs_cycle_time(ref, lag):
-    """The GFS forecast cycle time relative to another time.
+    """GFS forecast cycle time relative to another time.
 
     This function returns the datetime corresponding to a GFS forecast
     cycle time displaced by some number of hours relative to another
@@ -60,9 +38,23 @@ def relative_gfs_cycle_time(ref, lag):
         The relative GFS cycle time.
 
     """
-    lag = datetime.timedelta(hours=lag)
-    gfs = ref - lag
+    gfs = ref - timedelta(hours=lag)
     return gfs.replace(hour=gfs.hour//6*6,
                        minute=0,
                        second=0,
                        microsecond=0)
+
+def latest_gfs_cycle_time(lag=6):
+    """The most recent GFS cycle time.
+
+    Args:
+        lag: The default GFS forecast production lag is 6 hours.  To
+            get an earlier forecast, this might be set somewhat
+            shorter, with due care taken to ensure it isn't set too
+            short.
+
+    Returns:
+        The most recent GFS cycle time.
+
+    """
+    return relative_gfs_cycle_time(datetime.utcnow(), lag)
