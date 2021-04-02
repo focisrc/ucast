4e3592bb048a9a0efe074dbc08eed4785ee1f206
Chi-kwan Chan <chanc@arizona.edu>
2021-04-02 01:25:53 -0700

    Improve plot

--- mod/ucast/plot.py
+++ mod/ucast/plot.py
@@ -20,36 +20,39 @@ from matplotlib import pyplot as plt
 
 def plot_latest(dfs, title=None, name=None, **kwargs):
 
-    fig, axes = plt.subplots(6, 1, figsize=(8,8), sharex=True)
+    fig, axes = plt.subplots(6, 1, figsize=(12,12), sharex=True)
 
     for i, df in enumerate(dfs):
-        alpha = 1.0 if i == 0 else (1 - i/len(dfs)) / 5
+        alpha = 1.0 if i == 0 else (1 - i/len(dfs)) / 3
+        width = 1.0 if i == 0 else 0.5
         for j, ax in enumerate(axes):
-            ax.plot(df.date, df.iloc[:,j+1], alpha=alpha, **kwargs)
+            ax.plot(df.date, df.iloc[:,j+1],
+                    alpha=alpha, linewidth=width, **kwargs)
+            ax.axvline(x=dfs[0].date[0], linestyle=':', color='k')
 
     columns = dfs[0].columns
     for j, ax in enumerate(axes):
         ax.tick_params(axis='x',direction="in",top=True)
         ax.tick_params(axis='y',direction="in",right=True)
 
-    axes[0].set_ylabel(r'$\tau_{255}$')
-    axes[1].set_ylabel(r'$T_b$ [K]')
-    axes[2].set_ylabel(r'pwv [mm]')
-    axes[3].set_ylabel(r'lwp [kg m$^{-2}$]')
-    axes[4].set_ylabel(r'iwp [kg m$^{-2}$]')
-    axes[5].set_ylabel(r'o3 [DU]')
+    axes[0].set_ylabel(r'$\tau_{255}$',      fontsize=16)
+    axes[1].set_ylabel(r'$T_b$ [K]',         fontsize=16)
+    axes[2].set_ylabel(r'pwv [mm]',          fontsize=16)
+    axes[3].set_ylabel(r'lwp [kg m$^{-2}$]', fontsize=16)
+    axes[4].set_ylabel(r'iwp [kg m$^{-2}$]', fontsize=16)
+    axes[5].set_ylabel(r'o3 [DU]',           fontsize=16)
 
     axes[0].set_ylim(0, 1.1)
     axes[1].set_ylim(0, 330)
     axes[2].set_ylim(0, 16.5)
     axes[3].set_ylim(0, 2.2)
     axes[4].set_ylim(0, 2.2)
-    axes[5].set_ylim(230, 450)
+    axes[5].set_ylim(240, 460)
 
     if title is not None:
-        axes[0].set_title(title)
+        axes[0].set_title(title, fontsize=16)
 
-    axes[5].set_xlabel('Date')
+    axes[5].set_xlabel('Date', fontsize=16)
     axes[5].set_xlim(dfs[-1].date[0], None)
     plt.xticks(rotation=45, ha='right')
 
