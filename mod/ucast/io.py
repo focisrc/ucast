0baacc212ba1ea6f7d0f79866a43f9b068b7d8b2
Chi-kwan Chan <chanc@arizona.edu>
2021-04-01 23:12:51 -0700

    Implement read() to read back the forecast tables

--- mod/ucast/io.py
+++ mod/ucast/io.py
@@ -16,13 +16,20 @@
 # You should have received a copy of the GNU General Public License
 # along with `ucast`.  If not, see <http://www.gnu.org/licenses/>.
 
-import numpy as np
+import numpy  as np
+import pandas as pd
 
-dt_fmt  = "%Y%m%d_%H:%M:%S"
+names   = [             'date',    'tau',       'Tb',      'pwv',  'lwp',       'iwp',             'o3']
 heading = "#            date       tau225        Tb[K]      pwv[mm] lwp[kg*m^-2] iwp[kg*m^-2]       o3[DU]\n"
 out_fmt = "%16s %12.4e %12.4e %12.4e %12.4e %12.4e %12.4e"
+dt_fmt  = "%Y%m%d_%H:%M:%S"
 
 def save(file, df):
     with open(file, "w") as f:
         f.write(heading)
         np.savetxt(f, df.fillna(0).values, fmt=out_fmt)
+
+def read(file):
+    d = pd.read_csv(file, delim_whitespace=True, skiprows=1, names=names)
+    d.date = pd.to_datetime(d.date, format=dt_fmt)
+    return d
