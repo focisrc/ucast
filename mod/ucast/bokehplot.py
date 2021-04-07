3010cee57cb8b56ced7bc4edca30e85100b9f4c5
phanicode <63126072+phanicode@users.noreply.github.com>
2021-04-06 21:59:56 -0700

    imports added

--- mod/ucast/bokehplot.py
+++ mod/ucast/bokehplot.py
@@ -1,3 +1,8 @@
+import pandas as pd, matplotlib.pyplot as plt, numpy as np
+from bokeh.plotting import figure, output_file, show
+from bokeh.models import Range1d,Column
+
+
 def bokeh_static(): 
     names="date,tau225,Tb[K],pwv[mm],lwp[kg*m^-2],iwp[kg*m^-2],o3[DU]".split(",")
     sites=["ALMA", "APEX", "GLT", "JCMT", "KP", "LMT", "PDB", "PV", "SMA", "SMT", "SPT"]
