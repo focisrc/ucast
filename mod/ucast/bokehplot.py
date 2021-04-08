1320453861a959fbe3f5a6647d3ebdbde8b1a5ed
Chi-kwan Chan <chanc@arizona.edu>
2021-04-08 00:10:46 -0700

    Link the x-axes

--- mod/ucast/bokehplot.py
+++ mod/ucast/bokehplot.py
@@ -6,26 +6,33 @@ def bokeh_static(dfs, sites, fname):
               'violet', 'darkcyan', 'royalblue', 'seagreen', 'gold']
 
     output_file(fname+'.html')
-    plot_list = [create_plot(var, dfs, colors, sites)
-                 for var in "tau,pwv,lwp,iwp".split(",")]
-    p = Column(*plot_list)
-    show(p)
 
-def create_plot(var, dfs, colors, sites):
-    p2 = figure(title=var, plot_width=1600, plot_height=500, x_axis_type="datetime",
-                tools="pan,box_zoom,box_select,lasso_select,undo,wheel_zoom,redo,reset,save".split())
+    plots = []
+    for var in "tau,pwv,lwp,iwp".split(","):
+        if len(plots) == 0:
+            kwargs = {}
+        else:
+            kwargs = {'x_range':plots[0].x_range}
+        plots.append(create_plot(var, dfs, colors, sites, **kwargs))
+
+    show(Column(*plots))
+
+def create_plot(var, dfs, colors, sites, **kwargs):
+    p = figure(title=var, plot_width=1600, plot_height=500, x_axis_type="datetime",
+               tools="pan,box_zoom,box_select,lasso_select,undo,wheel_zoom,redo,reset,save".split(),
+               **kwargs)
     if var == "tau":
-        p2.y_range = Range1d(0, 1)
+        p.y_range = Range1d(0, 1)
     elif var == "pwv":
-        p2.y_range = Range1d(0, 15)
+        p.y_range = Range1d(0, 15)
     elif var == "lwp":
-        p2.y_range = Range1d(0, 1)
+        p.y_range = Range1d(0, 2)
     elif var == "iwp":
-        p2.y_range = Range1d(0, 2)
+        p.y_range = Range1d(0, 2)
     for i in range(11):
-        p2.line(dfs[i]['date'], dfs[i][var].fillna(0),
+        p.line(dfs[i]['date'], dfs[i][var].fillna(0),
                 color=colors[i], alpha=0.5, legend_label=sites[i])
     # Enable line hide toggle
-    p2.legend.location = "top_left"
-    p2.legend.click_policy = "hide"
-    return p2
+    p.legend.location = "top_left"
+    p.legend.click_policy = "hide"
+    return p
